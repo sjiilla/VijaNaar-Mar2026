f833d7f5a8862b810a68a97ddb6e8e61116aba1f
Sreeni Jilla <cnu.jilla@gmail.com>
2026-04-21 08:58:23 +0530

    Add PCA analysis to Titanic dataset
    Implement PCA on Titanic dataset for dimensionality reduction and visualize the results.

---
+++ ML/PCA/PCA1.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+@author: sjilla
+"""
+
+import os
+import pandas as pd
+from sklearn import decomposition #For PCA/Dimensionality Reduction
+import seaborn as sns
+
+#returns current working directory
+os.getcwd()
+#changes working directory
+os.chdir("C:/Data Science/Data")
+
+titanic_train = pd.read_csv("train.csv")
+
+#EDA
+titanic_train.shape
+titanic_train.info()
+
+titanic_train1 = pd.get_dummies(titanic_train, columns=['Pclass', 'Sex', 'Embarked'])
+titanic_train1.shape
+titanic_train1.info()
+
+X_train = titanic_train1.drop(['PassengerId','Age','Cabin','Ticket', 'Name','Survived'], axis=1, inplace=False)
+titanic_train1.shape
+X_train.info()
+
+#Here comes the PCA!
+pca = decomposition.PCA(n_components=2)
+pca.fit(X_train)
+
+print(pca.explained_variance_)
+print(pca.explained_variance_ratio_)
+print(pca.explained_variance_ratio_.cumsum())
+
+#Transformation of PCA happens here
+transformed_X_train = pca.transform(X_train)
+transformed_X_train.shape
+
+print(pca.explained_variance_)
+print(pca.explained_variance_ratio_)
+print(pca.explained_variance_ratio_.cumsum())
+
+y_train = titanic_train['Survived']
+
+#Assign transformed PCA data into new data frame for visualaiztion purpose
+transformed_df = pd.DataFrame(data = transformed_X_train, columns = ['pc1', 'pc2'])
+#See whethere PC1 and PC2s are orthogonal are not!
+sns.jointplot(x='pc1', y='pc2', data= transformed_df)
